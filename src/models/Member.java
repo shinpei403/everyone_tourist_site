6059200214c04e3c104a62c15afb3fb8eb64a9f5
shiom <shiom@192.168.0.16>
2022-05-28 14:42:16 +0900

    DTOモデル(Memberクラス)の作成

---
+++ src/models/Member.java
@@ -0,0 +1,84 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "hometown", nullable = false)
+    private String hometown;
+
+    @Column(name = "mail", nullable = false)
+    private String mail;
+
+    @Column(name = "password", length = 64, nullable = false)
+    private String password;
+
+    @Column(name = "adminFlag", nullable = false)
+    private Boolean adminFlag;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAdminFlag() {
+        return adminFlag;
+    }
+
+    public void setAdminFlag(Boolean adminFlag) {
+        this.adminFlag = adminFlag;
+    }
+
+
+}
